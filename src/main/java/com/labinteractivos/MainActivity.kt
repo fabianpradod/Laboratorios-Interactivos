02c5253fb955557ad3d9ebac7faa9b45da6dc5c5
fabianpradod <fabian.prado2004@gmail.com>
2024-10-20 18:32:28 -0600

    Update MainActivity.kt

--- src/main/java/com/labinteractivos/MainActivity.kt
+++ src/main/java/com/labinteractivos/MainActivity.kt
@@ -1,75 +1,52 @@
-package com.labinteractivos
+package com.laboratoriosinteractivos.lab10
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navOptions
-import com.labinteractivos.laboratorios.Lab8.charDetatils.CharDetailsDestination
-import com.labinteractivos.laboratorios.Lab8.charDetatils.navigateToCharDetailsScreen
-import com.labinteractivos.laboratorios.Lab8.characters.CharactersDestination
-import com.labinteractivos.laboratorios.Lab8.characters.charactersScreen
-import com.labinteractivos.laboratorios.Lab8.characters.navigateToCharactersScreen
-import com.labinteractivos.laboratorios.Lab8.homeLogIn.HomeLogInDestination
-import com.labinteractivos.laboratorios.Lab8.homeLogIn.homeLogInScreen
-import com.labinteractivos.ui.theme.LabInteractivosTheme
-import com.labinteractivos.laboratorios.Lab8.Character
-import com.labinteractivos.laboratorios.Lab8.charDetatils.charDetailsScreen
+import com.laboratoriosinteractivos.lab10.presentation.navigation.AppNavigation
+import com.laboratoriosinteractivos.lab10.ui.theme.RickAndMortyTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            LabInteractivosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = HomeLogInDestination,
+            val navController = rememberNavController()
+
+            RickAndMortyTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    AppNavigation(
                         modifier = Modifier
                             .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-                        homeLogInScreen(
-                            onHomeLogInClick = {
-                                navController.navigateToCharactersScreen(
-                                    destination = CharactersDestination,
-                                    navOptions =  navOptions {
-                                        popUpTo<HomeLogInDestination>{
-                                            inclusive = true
-                                            saveState = false
-                                        }
-                                        restoreState = false
-                                    }
-                                )
-                            }
-                        )
-                        charactersScreen (
-                            onCharactersClick = { character ->
-                                navController.navigateToCharDetailsScreen(
-                                    destination = CharDetailsDestination(
-                                        id = character.id
-                                    )
-                                )
-                            }
-                        )
-                        charDetailsScreen (
-                            onGoBackClick = {
-                                navController.navigateUp()
-                            }
-                        )
-                    }
+                            .padding(innerPadding),
+                        navController = navController
+                    )
                 }
             }
         }
